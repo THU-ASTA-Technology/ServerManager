8408e6ccb3cfd433b433d8bf8ba23a7408b14564
suikasxt <suikasxt@gmail.com>
2020-10-24 17:18:31 +0800

    debug_for_pycryptodome

--- crypto.py
+++ crypto.py
@@ -7,8 +7,8 @@ class myAES:
         #从asta.ini读入配置
         self.config = ConfigParser(allow_no_value=True)
         self.config.read(path, encoding="utf-8")
-        self.key = self.config.get('GPU_Server', 'KEY')
-        self.iv = self.config.get('GPU_Server', 'IV')
+        self.key = self.config.get('GPU_Server', 'KEY').encode('utf-8')
+        self.iv = self.config.get('GPU_Server', 'IV').encode('utf-8')
     
     def encrypt(self, start, end):
         #输入两个时间，加密为字符串
@@ -17,14 +17,13 @@ class myAES:
         if l>0:
             text += ' '*(16-l)
         self.aes = AES.new(self.key, AES.MODE_CBC, self.iv)
-        res = self.aes.encrypt(text)
+        res = self.aes.encrypt(text.encode('utf-8'))
         return res
     
     def decrypt(self, text):
         #输入密文解密为开始、结束时间
         self.aes = AES.new(self.key, AES.MODE_CBC, self.iv)
         text = self.aes.decrypt(text)
-        print(text)
         start, end = [datetime.fromtimestamp(float(a)) for a in text.split()]
         return start, end
 
